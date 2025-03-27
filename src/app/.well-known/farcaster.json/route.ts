ab8a78a995c13f855ad905c7f7a638149f08e058
Nishad Vadgama <nishad.vadgama@gmail.com>
2025-03-27 15:40:24 -0400

    refactor farcaster.json route to use environment variables for header, payload, and signature

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,10 +4,9 @@ export async function GET() {
   const config = {
     accountAssociation: {
       header:
-        "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
-      signature:
-        "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
+        process.env.NEXT_PUBLIC_FRAME_HEADER,
+      payload: process.env.NEXT_PUBLIC_FRAME_PAYLOAD,
+      signature: process.env.NEXT_PUBLIC_FRAME_SIG,
     },
     frame: {
       version: "1",
